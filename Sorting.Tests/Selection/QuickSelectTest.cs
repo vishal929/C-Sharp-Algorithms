5909d2beb581c7ac2c7e685ac96bbb4d74239fd0
vishal929 <vishal.patel929@gmail.com>
2025-03-10 20:14:21 -0400

    quick select working

---
+++ Sorting.Tests/Selection/QuickSelectTest.cs
@@ -0,0 +1,54 @@
+﻿using Sorting.ComparisonBased;
+using Sorting.Selection;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Abstractions;
+
+namespace Sorting.Tests.ComparisonBased;
+
+public class QuickSelectTest (ITestOutputHelper helper)
+{
+    [Fact]
+    public void TestSelectRecursive()
+    {
+        // create an array of size 10000
+        int[] test = new int[TestGlobalSize.testArraySize];
+
+        int[] testCopy = new int[TestGlobalSize.testArraySize];
+
+        for (int i = 0; i < test.Length; i++)
+        {
+            test[i] = Random.Shared.Next();
+        }
+
+        // copy the array to testCopy
+        Array.Copy(test, testCopy, test.Length);
+
+        // choose k randomly (k-th smallest)
+        int k = Random.Shared.Next(0, test.Length); 
+
+
+        // sort both and assert that the result is the same
+        var watch = new Stopwatch();
+        watch.Start();
+        int kVal = QuickSelect.Select(test, 0, test.Length - 1, k);
+        watch.Stop();
+
+        long selectMs = watch.ElapsedMilliseconds;
+
+        watch.Restart();
+        // sort the other array and assert the position values are the same 
+        Array.Sort(testCopy);
+        int kValTrue = testCopy[k];
+        watch.Stop();
+
+        Assert.Equal(kVal, kValTrue);
+
+        helper.WriteLine($"quick select recursive took {selectMs} ms while sorting with library and choosing k took: {watch.ElapsedMilliseconds} ms");
+
+    }
+}
---
+++ Sorting/Selection/QuickSelect.cs
@@ -0,0 +1,47 @@
+﻿using Sorting.ComparisonBased;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting.Selection;
+public class QuickSelect
+{
+    /// <summary>
+    /// Selects the k-th smallest element from the list (O(n) with quickselect)
+    /// </summary>
+    /// <typeparam name="T">returned element, throw exception if (k < 0 or k > list.Length)</typeparam>
+    /// <param name="list">list to filter</param>
+    /// <param name="k">largest indicator, i.e 1 for smallest, 2 for second smallest, etc.</param>
+    /// <returns>element representing the k-th smallest</returns>
+    public static T Select<T>(T[] list, int start, int end, int k) where T : IComparable<T>
+    {
+        // the k-th smallest will be at index k in the sorted array
+        // partition until we hit this
+
+        if (k < start || k > end) throw new ArgumentOutOfRangeException($"k:{k} is out of the bounds of the array with size: {list.Length}!");
+
+        // pick a random pivot and partition
+        int pivIndex = Random.Shared.Next(start, end+1);
+        T tmp = list[pivIndex];
+        list[pivIndex] = list[end];
+        list[end] = tmp;
+
+        int actualIndex = QuickSort.Partition(list, start, end);
+
+        if (actualIndex == k) return list[actualIndex];
+
+        if (k > actualIndex)
+        {
+            // recurse on right
+            return Select(list, actualIndex + 1, end, k);
+        } 
+        else
+        {
+            // recurse on left
+            return Select(list, start, actualIndex - 1,k);
+        }
+    }
+
+}

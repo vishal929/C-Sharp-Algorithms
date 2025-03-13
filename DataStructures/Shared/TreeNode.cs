498c4228c9016772499b2b052dac798d220bdae4
vishal929 <vishal.patel929@gmail.com>
2025-03-12 20:57:22 -0400

    start tree object

---
+++ DataStructures/Shared/TreeNode.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.Shared;
+
+public class TreeNode<T> where T : IComparable<T>
+{
+    TreeNode<T>?[] Children { get; init; }
+
+    T Value { get; init;}
+    public TreeNode(T value, int numChildren = 2)
+    {
+        // allocate space for children, unfilled children are null  
+        Children = new TreeNode<T>?[numChildren];
+        Value = value;
+    }
+
+    public void AddChild(TreeNode<T> child, int index)
+    {
+        if (index <0 || index >= Children.Length)
+        {
+            throw new ArgumentOutOfRangeException(nameof(index));
+        }
+
+        Children[index] = child;
+    }
+
+}
---
+++ DataStructures/Trees/BinaryTree.cs
@@ -0,0 +1,18 @@
+﻿using DataStructures.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.Trees;
+
+/// <summary>
+/// A binary tree is a tree where each node has a left and right child
+/// </summary>
+public class BinaryTree<T> where T : IComparable<T>
+{
+    TreeNode<T>? Root { get; set; }
+    
+     
+}

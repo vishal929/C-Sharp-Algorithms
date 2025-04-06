c30ac206aa7c096343c61dfe8431651b25fd526f
vishal929 <vishal.patel929@gmail.com>
2025-04-05 23:37:36 -0700

    add counting sort tests

---
+++ Sorting.Tests/NonComparisonBased/CountingSortTest.cs
@@ -0,0 +1,40 @@
+﻿using Sorting.NonComparisonBased;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Abstractions;
+
+namespace Sorting.Tests.NonComparisonBased;
+public class CountingSortTest (ITestOutputHelper helper)
+{
+    [Fact]
+    public void TestSort()
+    {
+        // need hundreds of millions of integers for counting sort to basically match lib sorts performance
+        int[] random = Enumerable.Range(0,120000000).Select(x => Random.Shared.Next(0,int.MaxValue/2)).ToArray();
+
+        // sort with counting sort
+        var watch = new Stopwatch();
+        watch.Start();
+        int[] randSorted = CountingSort.Sort(random);
+        watch.Stop();
+
+        helper.WriteLine($"Counting sort took {watch.ElapsedMilliseconds} ms");
+
+        // sort in place with lib sort
+        watch.Restart();
+        Array.Sort(random);
+        watch.Stop();
+
+        helper.WriteLine($"lib sort took {watch.ElapsedMilliseconds} ms");
+
+
+        // assert equality
+        Assert.True(random.SequenceEqual(randSorted));
+
+    }
+
+}
--- Sorting/NonComparisonBased/CountingSort.cs
+++ Sorting/NonComparisonBased/CountingSort.cs
@@ -4,9 +4,45 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Sorting.NonComparisonBased
+namespace Sorting.NonComparisonBased;
+public class CountingSort
 {
-    internal class CountingSort
+    // if we have nonnegative integers with known maximum, we can use an array to just count them and return an arranged version
+    // (if we have negative ints, we could possibly shift everything here by the negative lowest num, but that may introduce overflow)
+    public static int[] Sort(int[] arr)
     {
+        int max = arr.Max();
+        int[] counts = new int[max];
+        int numZero = 0;
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i] == 0)
+            {
+                numZero++;
+            } else
+            {
+                counts[arr[i] - 1]++;
+            }
+        }
+
+        int[] ret = new int[arr.Length];
+        int num = 0;
+
+        for (int j = 0; j < numZero; j++)
+        {
+            ret[num] = 0;
+            num++;
+        }
+
+        for (int j = 0; j < counts.Length; j++)
+        {
+            for (int k = 0; k < counts[j]; k++)
+            {
+                ret[num] = j+1;
+                num++;
+            }
+        }
+        return ret;
     }
+
 }

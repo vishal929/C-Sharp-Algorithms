b88229e7a46591bdb360f1756a3577b24f58830a
vishal929 <vishal.patel929@gmail.com>
2025-04-13 21:35:38 -0700

    add divide and conquer fast exponent

---
+++ Algorithms/DivideAndConquer.Tests/TestExponent.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Abstractions;
+
+namespace DivideAndConquer.Tests;
+
+public class TestExponent(ITestOutputHelper helper)
+{
+
+    [Fact]
+    public void TestPow()
+    {
+        double x = Random.Shared.NextDouble() * 30;
+        int pow = Random.Shared.Next(-10, 10);
+
+        double comp = Exponent.Pow(x, pow);
+        double lib = Math.Pow(x, pow);
+
+        // assert they are equal within a certain epsilon value
+        double epsilon = 0.05;
+
+        if (Math.Abs(lib - comp) > epsilon)
+        {
+            helper.WriteLine($"x: {x} power: {pow}");
+            helper.WriteLine("Our computed value " + comp);
+            helper.WriteLine("Library computed value " + lib);
+        }
+        
+
+        Assert.True(Math.Abs(lib-comp)<=epsilon);
+    }
+}
---
+++ Algorithms/DivideAndConquer/Exponent.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DivideAndConquer;
+public class Exponent
+{
+
+    /// <summary>
+    /// Computes the exponent of x to the specified power
+    /// </summary>
+    /// <param name="x">exponand</param>
+    /// <param name="power">exponent</param>
+    /// <returns>the result of the exponent</returns>
+    public static double Pow(double x, long power)
+    {
+        // T(n) = T(n/2) + O(1) -> Log(n) exponent
+        // x^n = x^{n/2} * x^{n/2}
+        if (power  == 0) return 1;
+        if (power == 1) return x;
+        if (power == -1) return 1 / x;
+
+        // if power is not even, subtract 1 from it and then recurse
+        if (power % 2 != 0)
+        {
+            power--;
+            long half = power / 2;
+            double comp = Pow(x, half);
+            return x * comp * comp;
+        }
+        else
+        {
+            long half = power / 2;
+            double comp = Pow(x, half);
+            return comp * comp;
+        }
+    }
+}

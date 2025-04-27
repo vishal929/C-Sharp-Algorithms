9b5f6fdd943ac5cf6ff5f14bde91544e66398b9d
vishal929 <vishal.patel929@gmail.com>
2025-04-27 12:20:43 -0700

    implemented sieve of eratosthenes prime generation

---
+++ Algorithms/DynamicProgramming.Tests/TestPrimes.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Abstractions;
+
+namespace DynamicProgramming.Tests;
+public class TestPrimes(ITestOutputHelper helper)
+{
+    [Fact]
+    public void TestGeneratePrimes()
+    {
+        int n = 50000;
+
+        Stopwatch watch = new();
+        watch.Start();
+        int[] sievePrimes = SieveOfEratosthenes.GeneratePrimes(n);
+        watch.Stop();
+
+        helper.WriteLine($"Sieve of Eratosthenes took {watch.ElapsedMilliseconds} milliseconds");
+
+        watch.Restart();
+        int[] brutePrimes = PrimesBrute(n);
+        watch.Stop();
+
+        helper.WriteLine($"brute force took {watch.ElapsedMilliseconds} milliseconds");
+
+
+        Assert.Equal(brutePrimes, sievePrimes);
+    }
+
+
+
+    /// <summary>
+    /// Generate primes by brute force trial division
+    /// </summary>
+    /// <param name="n"></param>
+    /// <returns></returns>
+    public int[] PrimesBrute(int n)
+    {
+        List<int> primes = new List<int>();
+        primes.Add(2);
+
+        for (int i = 3; i <= n; i++)
+        {
+            bool isPrime = true;
+            for (int j = 0; j < primes.Count(); j++)
+            {
+                if (i % primes[j] == 0)
+                {
+                    // this isnt prime
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) primes.Add(i);
+        }
+        return primes.ToArray();
+    }
+}
---
+++ Algorithms/DynamicProgramming/SieveOfEratosthenes.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicProgramming;
+public class SieveOfEratosthenes
+{
+    /// <summary>
+    /// Generates primes up to n
+    /// </summary>
+    /// <param name="n">upper bound (inclusive) on primes to generate</param>
+    /// <returns>list of primes</returns>
+    public static int[] GeneratePrimes(int n)
+    {
+        // just use n+1 so we can use 1 indexing
+        bool[] possible = new bool[n+1];
+        for (int i = 2; i <= (int)Math.Sqrt(n); i++)
+        {
+            // if this is marked, continue (its composite)
+            if (possible[i])
+            {
+                // composite continue
+                continue;
+            }
+            else
+            {
+                // this is prime, mark the composites
+                // O(n/i) time
+                // the partial prime harmonic series is bounded by O(loglog(n))
+                for (int j=i*i;j<=n; j += i)
+                {
+                    possible[j] = true;
+                }
+            }
+        }
+
+        // in the above block ,we do n/2 + n/3 + n/5 + ..... + n/lastPrime work.
+        // so this total work will be O(nloglog(n))
+
+        // skip 0 and 1 and return the primes (O(n))
+        int numPrime = 0;
+        for (int i = 2; i <= n; i++)
+        {
+            numPrime += (possible[i] ? 0 : 1);
+        }
+
+        int[] primes = new int[numPrime];
+        int c = 0;
+        for (int i = 2; i <= n; i++)
+        {
+            if (!possible[i])
+            {
+                primes[c] = i;
+                c++;
+            }
+        }
+        return primes;
+
+        // we have total complexity O(n loglog(n))
+
+
+    }
+}

f4c58ed549315902f79e11ed528ba7a6945e0f02
vishal929 <vishal.patel929@gmail.com>
2025-04-27 12:51:34 -0700

    start heap stuff

---
+++ DataStructures/Heaps/BinaryHeap.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.Heaps;
+public class BinaryHeap<T> where T: IComparable<T>
+{
+    // the backing store for the binary heap is a list
+    private IList<T> ListInternal { get; } = new List<T>();
+
+    public int Count() => ListInternal.Count;
+    
+    public BinaryHeap(bool isMax)
+    {
+        // if isMax is true, create a max binary heap, else create a min heap
+    }
+
+    public T Peek()
+    {
+        if (ListInternal.Count == 0) throw new InvalidOperationException("No elements are in the heap");
+        return ListInternal[0];
+    } 
+
+    
+
+}

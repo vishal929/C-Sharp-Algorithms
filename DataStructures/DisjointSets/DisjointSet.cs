d40853edd31135bac88cf9bb6fd2dc3cf5864005
vishal929 <vishal.patel929@gmail.com>
2025-05-11 13:58:45 -0700

    implement disjoint set

---
+++ DataStructures/DisjointSets/DisjointSet.cs
@@ -0,0 +1,43 @@
+﻿using DataStructures.Shared;
+
+namespace DataStructures.DisjointSets;
+public class DisjointSet
+{
+
+    public DisjointSetNode<T> MakeSet<T>(T item)
+    {
+        return new DisjointSetNode<T>(item);
+
+    }
+
+    public void Union<T>(DisjointSetNode<T> first, DisjointSetNode<T> second)
+    {
+        Link(FindSet<T>(first), FindSet<T>(second)); 
+    }
+
+    public DisjointSetNode<T> FindSet<T>(DisjointSetNode<T> item)
+    {
+        if (item != item.Parent)
+        {
+            // path compression
+            item.Parent = FindSet<T>(item.Parent);
+        }
+        return item.Parent;
+    }
+
+    private void Link<T>(DisjointSetNode<T> first, DisjointSetNode<T> second)
+    {
+        if (first.Rank > second.Rank)
+        {
+            second.Parent = first;
+        }
+        else
+        {
+            first.Parent = second;
+            if (first.Rank == second.Rank)
+            {
+                second.Rank++;
+            }
+        }
+    }
+}
---
+++ DataStructures/Shared/DisjointSetNode.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.Shared;
+public class DisjointSetNode<T>
+{
+    public T Value { get;  }
+
+    public DisjointSetNode<T> Parent { get; set; } 
+
+    public int Rank { get; set; }
+
+    public DisjointSetNode(T Value)
+    {
+        this.Value = Value;
+        Parent = this;
+        Rank = 0;
+    }
+
+}
